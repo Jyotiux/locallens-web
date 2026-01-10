21ab6c2b2fb12df090cc7710debc9c9d129fff67
Jyoutis <159123869+Jyotiux@users.noreply.github.com>
2026-01-10 23:05:20 +0530

    Update App.jsx

--- myProject/src/App.jsx
+++ myProject/src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import MapView from "./components/ViewMap.jsx";
 import SpotForm from "./components/SpotForm.jsx";
-
+//
 const API_BASE_URL = "https://locallens-8r2k.onrender.com/api";
 
 const App = () => {
