df75d3ad9dd5466255601d86b27c7da406a3bd5e
Jyoutis <159123869+Jyotiux@users.noreply.github.com>
2026-09-12 23:07:24 +0530

    Update API base URL for the application

--- myProject/src/App.jsx
+++ myProject/src/App.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import MapView from "./components/ViewMap.jsx";
 import SpotForm from "./components/SpotForm.jsx";
 //
-const API_BASE_URL = "https://locallens-8r2k.onrender.com/api";
+const API_BASE_URL = "https://locallens-fn9b.onrender.com/api";
 
 const App = () => {
   const [selectedCoords, setSelectedCoords] = useState(null);
